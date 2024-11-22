2762d898a3d06cb6af43c96844ba98b0f08e0c0a
Niva Phueklieng <dphuekli@uci.edu>
2024-11-22 11:56:31 -0800

    Side navigation bar for animals memory card game

--- js/animals.js
+++ js/animals.js
@@ -148,3 +148,16 @@ function goToMainMenu() {
 // Add event listeners
 cards.forEach((card) => card.addEventListener('click', flipCard));
 resetButton.addEventListener('click', resetGame);
+
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.body.style.backgroundColor = "white";
+}
